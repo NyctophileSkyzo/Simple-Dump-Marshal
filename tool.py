f92a79ac7c083ca68259881a1d2881036023f32c
Ｄｅｎｖｅｎｔａ <164549259+NathVaskyloClearesta@users.noreply.github.com>
2024-04-14 11:40:13 +0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,26 +1,35 @@
 import marshal
 import dis
 
-__import__('os').system('cls' if 'win' in __import__('sys').platform.lower() else 'clear')
+__import__("os").system("cls" if "win" in __import__("sys").platform.lower() else "clear")
 
-print(f"""# Simple Code for Dump Marshal
-# Coded by: Denventa Khurayra X.\n""")
+print(
+    f"""# Simple Code for Dump Marshal
+# Coded by: Denventa Khurayra X.\n"""
+)
 a = input(f"File path: ")
 mode = int(input(f"Mode 1\\2: "))
-with open(a, 'r') as tai:
+with open(a, "r") as tai:
     __ = tai.read()
 b = marshal.loads
+
+
 def hello(*aa):
     __ = b(*aa)
     dis.dis(__)
     return __
+    __import__("sys").exit
+
+
 def kontol(*aa):
     __ = b(*aa)
-    #print(f"{aa[0]}") # Simple Code for Dump Marshal
-    print(f"exec(__import__('marshal').loads({aa[0]}))") # Just understand it yourself
+    # print(f"{aa[0]}") # Simple Code for Dump Marshal
+    print(f"exec(__import__('marshal').loads({aa[0]}))")  # Just understand it yourself
     return __
+
+
 if mode == 1:
     marshal.loads = hello
 else:
     marshal.loads = kontol
-exec(__)
+# exec(__)
