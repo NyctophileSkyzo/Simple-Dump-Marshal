0b9d717ae96504adafb5aec1e2ce225e48d45924
Nyctophile Skyzofrenia IV <164549259+NathVaskyloClearesta@users.noreply.github.com>
2024-04-21 09:48:28 +0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,1 +1,36 @@
-import marshalimport dis__import__("os").system("cls" if "win" in __import__("sys").platform.lower() else "clear")print(    f"""# Simple Code for Dump Marshal# Coded by: Denventa Khurayra X.\n""")a = input(f"File path: ")mode = int(input(f"Mode 1\\2: "))with open(a, "r") as tai:    __ = tai.read()b = marshal.loadsdef hello(*aa):    __ = b(*aa)    dis.dis(__)    #    return __    __import__("sys").exit()def kontol(*aa):    __ = b(*aa)    # print(f"{aa[0]}") # Simple Code for Dump Marshal    print(f"exec(__import__('marshal').loads({aa[0]}))")  # Just understand it yourself    __import__("sys").exit()if mode == 1:    marshal.loads = helloelse:    marshal.loads = kontolexec(__)__import__("sys").exit()
+import marshal
+import dis
+
+__import__("os").system("cls" if "win" in __import__("sys").platform.lower() else "clear")
+
+print(
+    f"""# Simple Code for Dump Marshal
+# Coded by: Denventa Khurayra X.\n"""
+)
+a = input(f"File path: ")
+mode = int(input(f"Mode 1\\2: "))
+with open(a, "r") as tai:
+    __ = tai.read()
+b = marshal.loads
+
+
+def hello(*aa):
+    __ = b(aa[0])
+    dis.dis(__)
+    __import__("sys").exit()
+
+
+def kontol(*aa):
+    __ = b(aa[0])
+    marshalled_code = marshal.dumps(__)
+    for _ in range(150):
+        print(f"exec(__import__('marshal').loads({repr(marshalled_code)}))")
+    dis.dis(__)
+    __import__("sys").exit()
+
+
+if mode == 1:
+    marshal.loads = hello
+else:
+    marshal.loads = kontol
+exec(__)
