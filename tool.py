79c556c6fc4916ca18d07c85a0fed97279b0b992
Nyctophile Skyzofrenia IV <164549259+NathVaskyloClearesta@users.noreply.github.com>
2024-04-21 10:48:23 +0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -11,26 +11,29 @@ a = input(f"File path: ")
 mode = int(input(f"Mode 1\\2: "))
 with open(a, "r") as tai:
     __ = tai.read()
-b = marshal.loads
+x = marshal.loads
+xx = exec
 
 
-def hello(*aa):
-    __ = b(aa[0])
+def dump(*aa):
+    __ = x(aa[0])
     dis.dis(__)
+    print(f"{aa[0]}")
+    return __  # or (#) return __
     __import__("sys").exit()
 
 
-def kontol(*aa):
-    __ = b(aa[0])
-    marshalled_code = marshal.dumps(__)
-    for _ in range(150):
-        print(f"exec(__import__('marshal').loads({repr(marshalled_code)}))")
+def print_dump(*bb):
+    __ = x(bb[0])
     dis.dis(__)
+    marshalled_code = marshal.dumps(__)
+    print(f"exec(__import__('marshal').loads({repr(marshalled_code)}))")
+    return __
     __import__("sys").exit()
 
 
 if mode == 1:
-    marshal.loads = hello
+    marshal.loads = dump
 else:
-    marshal.loads = kontol
+    marshal.loads = print_dump
 exec(__)
